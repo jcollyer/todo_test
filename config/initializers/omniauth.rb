ddbe1ef1118a9c0cf020a4ed3a76a02b338e5db0
jcollyer <collyerdesign@gmail.com>
2013-06-01 12:38:04 -0700

    Created User model and validations.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #  provider :developer unless Rails.env.production?
+  provider :developer unless Rails.env.production?
   provider :twitter, ENV['7d8H8xbgTCUYvDmIFaPZOA'], ENV['OUiq9EEWHaEtfUc2lEgwn9f3enWcwW3Dw0Xzpmk1o']
 end
