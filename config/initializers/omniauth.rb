82afc27173ab31cbe425f291800885fd63e893e5
jcollyer <collyerdesign@gmail.com>
2013-06-01 13:11:29 -0700

    Change oauth initializer to use ENV keys. add twitter link to home

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter, ENV['7d8H8xbgTCUYvDmIFaPZOA'], ENV['OUiq9EEWHaEtfUc2lEgwn9f3enWcwW3Dw0Xzpmk1o']
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
 end
