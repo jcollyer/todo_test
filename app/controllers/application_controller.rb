c521be47d215aec2602d3abe2467b673af26a225
jcollyer <collyerdesign@gmail.com>
2013-06-01 14:50:23 -0700

    Twitter callback route and locgic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,32 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Render the homepage.
+   def homepage; end
+
+   # Public: The currently signed in user or nil.
+   #
+   # Returns a User or NilClass.
+   def current_user
+     @current_user
+   end
+
+   helper_method :current_user
+
+  # Internal: Set the currently logged in user for request and session.
   def current_user
-    nil
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+   # Internal: Set the currently logged in user for request and session.
+   def set_current_user_for_request_and_session(user)
+     @current_user = user
+     session[:user_id] = user.id
+   end
+
+   # Internal Clear the current user for request and session.
+   def clear_current_user_for_request_and_session
+     @current_user = nil
+     session[:user_id] = nil
+   end
 end
--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,5 +1,8 @@
 class TwitterController < ApplicationController
   def callback
+    user = User.from_omniauth(omniauth)
+    self.current_user = user
 
+    redirect_to root_path
   end
 end
