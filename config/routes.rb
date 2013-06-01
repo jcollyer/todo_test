30d7c3ef07b77789624e2357ab4d2e36030d1dd1
jcollyer <collyerdesign@gmail.com>
2013-06-01 12:52:51 -0700

    add current_user in app controller and links on home.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Todo::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static#index'
 
+  get 'twitter/callback'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
